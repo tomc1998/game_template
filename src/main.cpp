4810624c379f57a5c715367e919975bc158c3bb4
Thomas Cheng <thomascheng1998@googlemail.com>
2019-02-09 21:17:36 +0000

    Added clang complete example and fixed compilation for clang

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <entt/entt.hpp>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -39,6 +41,7 @@ int main(int argc, char** argv) {
 
   // Create state
   entt::DefaultRegistry registry;
+
   for(auto ii = 0; ii < 4; ++ii) {
     auto e = registry.create();
     unsigned char color[4] {5, 5, (unsigned char)((rand() % 30) + 20), 255};
